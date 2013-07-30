57db4a77989bc14e11f0f718489733daae5c494f
imranshaikh <imran@adshub.net>
2013-07-30 13:04:50 +0530

    Redo code in on Method

--- apps/smsway/smsway.rb
+++ apps/smsway/smsway.rb
@@ -1,24 +1,27 @@
 module Smsway
   module EventHandler
     def ticket_created
-      return true unless settings.notify_ticket_created.to_s = '1'
-      sent_ticket_sms(payload.ticket)
+      return true unless settings.notify_ticket_created.to_s == '1'
+      ticket = payload.ticket
+      sent_sms("New ticket from #{ticket.requester.name || ticket.requester.email} - #{ticket.subject}")
     end
 
     def customer_reply_created
       return true unless settings.notify_customer_reply_created.to_s == '1'
       reply = payload.reply
-      sent_reply_sms(payload.ticket, payload.reply)
+      sent_sms("New customer reply from #{reply.replier.name || reply.replier.email} in #{payload.ticket.subject}")
     end
 
     def agent_reply_created
       return true unless settings.notify_agent_reply_created.to_s == '1'
-      sent_reply_sms(payload.ticket, payload.reply)
+      reply = payload.reply
+      sent_sms("New agent reply from #{reply.replier.name || reply.replier.email} in #{payload.ticket.subject}")
     end
 
     def comment_created
       return true unless settings.notify_comment_created.to_s == '1'
-      sent_comment_sms(payload.ticket, payload.comment)
+      comment = payload.comment
+      sent_sms("New comment from #{comment.commenter.name || comment.commenter.email} in #{payload.ticket.subject}")
     end
   end
 end
@@ -44,28 +47,7 @@ module Smsway
 
     white_list :notify_ticket_created
 
-    def sent_ticket_sms(ticket)
-      message = "New ticket from #{ticket.requester.name || ticket.requester.email} - #{ticket.subject}"
-      http_post "http://sent.ly/command/sendsms" do |req|
-        req.params[:username] = settings.username
-        req.params[:password] = settings.password
-        req.params[:to]       = "+#{settings.number}"
-        req.params[:text]     = message[0...160]
-      end
-    end
-
-    def sent_reply_sms(ticket, reply)
-      message = "New reply from #{reply.replier.name || reply.replier.email} in #{ticket.subject}"
-      http_post "http://sent.ly/command/sendsms" do |req|
-        req.params[:username] = settings.username
-        req.params[:password] = settings.password
-        req.params[:to]       = "+#{settings.number}"
-        req.params[:text]     = message[0...160]
-      end
-    end
-
-    def sent_comment_sms(ticket, comment)
-      message = "New comment from #{comment.commenter.name || comment.commenter.email} in #{ticket.subject}"
+    def sent_ticket_sms(message)
       http_post "http://sent.ly/command/sendsms" do |req|
         req.params[:username] = settings.username
         req.params[:password] = settings.password
